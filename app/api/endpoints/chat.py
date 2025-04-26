da810ef08342f47e0446eaddb9463f7779328e2a
João Pedro Resende <jpresende502@gmail.com>
2025-04-26 19:16:08 -0300

    add: body to get query from user

--- app/api/endpoints/chat.py
+++ app/api/endpoints/chat.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 from ...services.llm import ( generate_response)
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class Query(BaseModel):
+    query: str
+
+
 @router.post("/chat")
-def chat(query):
-    response = generate_response(query)
+def chat(query: Query):
+    response = generate_response(query.query)
 
     return {"message": response}
